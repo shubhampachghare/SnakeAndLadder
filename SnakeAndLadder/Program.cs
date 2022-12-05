d521c484ef84fa5d6f1d448c55e0cc32e016dd6c
shubhampachghare <shubhampachghare@gmail.com>
2022-12-05 22:15:41 +0530

    [shubham]- Welcome to snake and ladder

---
+++ SnakeAndLadder/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SnakeAndLadder
+{
+    class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to snake and ladder");
+        }
+    }
+}
