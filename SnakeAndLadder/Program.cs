25d94ee96d2b32aa407a20d2aece23dbf9f2e290
shubhampachghare <shubhampachghare@gmail.com>
2022-12-05 22:19:30 +0530

    [shubham]- Initialize the position of player

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -4,10 +4,14 @@ namespace SnakeAndLadder
 {
     class Program
     {
-      
+
+        const int START_POISTION = 0;
+        const int END_POSTION = 100;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to snake and ladder");
+            Console.WriteLine("start postion is: " + START_POISTION);
+            Console.WriteLine("end postion is: " + END_POSTION);
         }
     }
 }
